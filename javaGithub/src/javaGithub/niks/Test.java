6c5dd88457b703a14d986b7ae28594fce93bf70b
AkashW0712 <akashwankhede105@gmail.com>
2024-07-10 14:28:28 +0530

    add m1 method.java
    adding m1 method for printing statment

--- javaGithub/src/javaGithub/niks/Test.java
+++ javaGithub/src/javaGithub/niks/Test.java
@@ -1,10 +1,13 @@
 package javaGithub.niks;
 
 public class Test {
-
+           public void m1(){
+		   System.out.println("m1...method");
+	   }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("nikhil Bad boy");
 	}
+
 
 }

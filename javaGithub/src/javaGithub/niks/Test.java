a8af1fb9641940941090d17df4e0c5a06a789b0c
AkashW0712 <akashwankhede105@gmail.com>
2024-07-10 15:08:50 +0530

    add m2 method

--- javaGithub/src/javaGithub/niks/Test.java
+++ javaGithub/src/javaGithub/niks/Test.java
@@ -4,7 +4,11 @@ public class Test {
            public void m1(){
 		   System.out.println("m1...method");
 	   }
-	public static void main(String[] args) {
+
+	public void m2()
+	{
+	System.out.println(m2 print)	
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("nikhil Bad boy");
 	}
